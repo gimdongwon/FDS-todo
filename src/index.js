e2a67f0537d7306956c11267f8911eb66fc861e2
kimdongwon <ehddnjs8989@gmail.com>
2018-06-03 12:41:10 +0900

    개인프로젝트로 변경

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
+import axios from "axios";
+const postAPI = axios.create({
+  baseURL: process.env.API_URL
 });
+
+const templates = {
+  aboutItems: document.querySelector("#about__items").content,
+}
+
+function render (fragment){
+  rootEl.textContent = '';
+  rootEl.appendChild(frag);
+}
+
+function about () {
+  const fragment = document.importNode(templates.aboutItems, true)
+  document.querySelector(".about").addEventListener("click", e=>{
+    console.log("about")
+    document.querySelector("#about__items").appendChild(fragment)
+  })
+}
+
+function works(){
+  const fragment = document.importNode(templates.aboutItems, true)
+  document.querySelector(".works").addEventListener("click", e => {
+    console.log("works")
+    let child = document.querySelector(".about__items");
+    child.removeChild(fragment)
+  })
+}
+
+  about();
+  works();
